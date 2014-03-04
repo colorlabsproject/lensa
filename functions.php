bcff88f33fb87b6b0c0d1cc88941f6700b23b314
Arif Widipratomo <snakepliskinz@gmail.com>
2014-03-04 11:59:43 +0700

    Fix: Image with Caption inside Post Content is not Responsive

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 /*-----------------------------------------------------------------------------------*/
 error_reporting(0);
 
+if ( ! isset( $content_width ) ) $content_width = 612;
+
 // Set path to ColorLabs Framework and theme specific functions
 $functions_path = TEMPLATEPATH . '/functions/';
 $includes_path = TEMPLATEPATH . '/includes/';
